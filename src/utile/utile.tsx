ea9573383d63591f6557be0f0d3b59567dfb568b
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-03-15 20:11:28 +0200

    commit cu afisare rezultat loadconfig 15032025

--- src/utile/utile.tsx
+++ src/utile/utile.tsx
@@ -2,6 +2,7 @@ import { AppConfig } from '../model/AppConfig';
 
 export async function loadConfig(): Promise<AppConfig> {
     const response = await fetch('/config/config.json');
+    console.log(response);
     if (!response.ok) {
         console.error('Failed to load configuration', response.statusText);
         return { BASE_URL: '' };
