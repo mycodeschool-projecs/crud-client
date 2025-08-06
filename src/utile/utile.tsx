d609dd20f9ab7f70a3cc7164557fdc02ac409a48
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-08-06 16:48:53 +0300

    fixed loadconfig method

--- src/utile/utile.tsx
+++ src/utile/utile.tsx
@@ -5,7 +5,7 @@ export async function loadConfig(): Promise<AppConfig> {
     console.log(response);
     if (!response.ok) {
         console.error('Failed to load configuration', response.statusText);
-        return { BASE_URL: '' };
+        return { BASE_URL: '',KEYCLOAK_URL:'' };
     }
     const config: AppConfig = await response.json();
     return config;
